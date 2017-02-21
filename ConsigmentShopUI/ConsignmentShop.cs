667c99025bd6addcbab5bc5a9c9ec010d82fc1c5
Dragos Andrei Olaru <DragosAndreiOlaru@gmail.com>
2017-02-21 20:41:48 +0200

    modified 3 Classes

--- ConsigmentShopUI/ConsignmentShop.cs
+++ ConsigmentShopUI/ConsignmentShop.cs
@@ -14,16 +14,29 @@ namespace ConsigmentShopUI
     public partial class ConsignmentShop : Form
     {
         private Store store = new Store();
-
         public ConsignmentShop()
         {
             InitializeComponent();
+            SetupData();
+        }
+        public void SetupData()
+        {
+            store.Vendors.Add(new Vendor { FirstName = "Bill", LastName = "Smith" });
+            store.Vendors.Add(new Vendor { FirstName = "Sue", LastName = "Jones" });
         }
 
-       private void SetupData()
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
